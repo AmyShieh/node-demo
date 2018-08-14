a5159575ef35b8f849fd58b4e1c35dae730e511f
xiechangqing <576535019@qq.com>
2018-08-14 22:18:27 +0800

    update

--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,8 @@
 /**
  * Created by amyqing719@gmail.com on 13/08/2018.
  */
+
+var person = require('./person');
 console.log('hello node');
+console.log(person.name);
+console.log(person.age);
---
+++ demo/person.js
@@ -0,0 +1,7 @@
+/**
+ * Created by amyqing719@gmail.com on 14/08/2018.
+ */
+var name = 'amy';
+var age = 24;
+
+exports.name = name;
