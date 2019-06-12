814e7b6820bd038ad4bf1c809188d86b42049e50
aliveAmy <aliveAmy719@gmail.com>
2019-06-12 22:59:23 +0800

    update

--- express-demo.js
+++ express-demo.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
 const app = express();
+const bodyParse = require('body-parser');
+
+const urlEncodedParser = bodyParse.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
   res.send('this is homepage')
@@ -12,8 +16,17 @@ app.get('/contact', (req, res) => {
   res.sendFile(__dirname+`/404.html`)
 });
 
-app.get('/profile/:id', (req, res) => {
-  res.render('profile', { id: req.params.id });
+app.get('/profile', (req, res) => {
+  res.render('profile', { id: 'aaa' });
 });
+
+app.post('/profile', urlEncodedParser, (req, res) => {
+  console.log(req.body);
+  res.render('profile', { id: 'aaa' });
+});
+
+app.use('/profile/:id', (req, res, next) => {
+  console.log(req.url);
+})
 
 app.listen('3000');
